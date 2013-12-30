4a62bd8dabdd16fc67b86c0405487658ce1d1599
samui13 <ss.to13@gmail.com>
2013-12-31 04:27:39 +0900

    Com One Scene

--- 2013-12-27-220244.js
+++ 2013-12-27-220244.js
@@ -20,7 +20,7 @@
 
 	    //this.camera.position = new THREE.Vector3(400,300,700);
 	    this.camera.position = new THREE.Vector3(0,0,100);
-	    //this.camera.position = new THREE.Vector3(0,0,50);
+	    //this.camera.position = new THREE.Vector3(0,500,0);
 	    this.camera.lookAt(new THREE.Vector3(0,0,0));
 
 	    this.renderer.setSize(this.width,this.height);
@@ -91,7 +91,12 @@
 	    return this.WordGeometry[id];
 	}),
     };
-    
+    var animeList = (function(){
+	function F() {}
+	F.prototype = Array();
+	F.prototype.prototype = {'test':"animeList"};
+	return  new F();
+    });
     TEST = {
 	'geo': new THREE.Geometry(),
 	'container': new THREE.Object3D(),
@@ -107,30 +112,255 @@
 	    //this.container.position.y = -60;
 	    App.scene.add(this.container);
 	    App.reload();
-
 	}),
     };
+    var groupGeometry = (function(){
+	function F() {
+	    this.geo = new THREE.Geometry();
+	    //this.container = new THREE.Object3D();
+	    this.mesh = new THREE.Mesh(this.geo,new THREE.MeshLambertMaterial({'color':'#f00'}));
+	    this.add(this.mesh);
+	    this.merge = (function(obj){
+		THREE.GeometryUtils.merge(this.geo,obj);
+	    });
+	};
+	F.prototype = new THREE.Object3D();
+	return new F();
+    });
     App.initRender();
     //App.initObject();
     App.reload();
+    
+    var motions = new animeList();
+    console.log(motions.prototype.test);
+
     var temp = SceneObj.AddWord(10,'東中出身',new THREE.Vector3(-28,0,0));
     App.scene.add(temp);
-
-    var tween0 = new TWEEN.Tween(temp.position).to({z:30},1000)
-        .easing(TWEEN.Easing.Elastic.Out);
-    var tween2= new TWEEN.Tween(temp.rotation).to({z:Math.PI/2},200)
-	.easing(TWEEN.Easing.Linear.None);
-    var tween3= new TWEEN.Tween(temp.position).to({y:-27},200)
-	.easing(TWEEN.Easing.Linear.None)
-	.onStart(function(){
-	    tween2.start();
-	});
+    var temp = SceneObj.AddWord(60,'涼宮ハルヒ',new THREE.Vector3(150,-32,30));
+    //var t = SceneObj.AddWord(30,'ただの',new THREE.Vector3(temp.position.x,temp.position.y+10,-45));
+    SceneObj.AddWord(30,'ただの',new THREE.Vector3(0,0,0));
+    SceneObj.AddWord(100,'人間',new THREE.Vector3(0,0,0));
+    SceneObj.AddWord(30,'には',new THREE.Vector3(0,0,0));
+    SceneObj.AddWord(100,'興味',new THREE.Vector3(0,0,0));
+    SceneObj.AddWord(40,'ありません。',new THREE.Vector3(0,0,0));
     
-    tween0.chain(tween3);
-    tween0.start();
+    motions.push(new TWEEN.Tween(SceneObj.getGeometry(0).position)
+		 .to({z:30},1000)
+		 .easing(TWEEN.Easing.Elastic.Out));
+    motions.push(new TWEEN.Tween(SceneObj.getGeometry(0).rotation)
+		 .to({z:Math.PI/2+0.3},200)
+		 .easing(TWEEN.Easing.Linear.None));
+    
+    motions.push(new TWEEN.Tween(SceneObj.getGeometry(0).position)
+		 .to({y:-27},200)
+		 .easing(TWEEN.Easing.Linear.None)
+		 .onStart(function(){
+		     motions[1].start();
+		     motions[3].start();
+		     App.scene.add(SceneObj.getGeometry(1));
+		 }));
+    var temp = new groupGeometry();
+    var temp1 = new groupGeometry();
+    motions.push(new TWEEN.Tween(SceneObj.getGeometry(1).position)
+		 .to({x:-30},150)
+		 .easing(TWEEN.Easing.Linear.None)
+		 .onStart(function(){
+		     
+		 })
+		.onComplete(function(){
+		    App.scene.remove(SceneObj.getGeometry(0));
+		    App.scene.remove(SceneObj.getGeometry(1));
+		    temp.merge(SceneObj.getGeometry(0));
+		    temp.merge(SceneObj.getGeometry(1));
+		    App.scene.add(temp);
+		    motions[3].chain(motions[4]);
+		}));
+    motions.push(new TWEEN.Tween(temp.position)
+		 .to({x:-400},800)
+		 .easing(TWEEN.Easing.Linear.None)
+		 .onComplete(function(){
+		     motions[5].start();
+		     motions[6].start();
+		     //motions[4].chain(motions[5]);
+		     //motions[4].chain(motions[6]);
+		 }));
+    motions.push(new TWEEN.Tween(temp.rotation)
+		 .to({z:-Math.PI/2},100)
+		 .easing(TWEEN.Easing.Linear.None)
+		 .onComplete(function(){
+		 }));
+    motions.push(new TWEEN.Tween(temp.position)
+		 .to({x:-150,y:180,z:-500},300)
+		 .easing(TWEEN.Easing.Linear.None)
+		 .onComplete(function(){
+		 }));
+    // 7
+    motions.push(new TWEEN.Tween(temp.position)
+		 .to({x:-50,y:0,z:-100},300)
+		 .delay(800)
+		 .easing(TWEEN.Easing.Linear.None)
+		 .onStart(function(){
+		     motions[8].start();
+		     //motions[9].start();
+		 })
+		 .onComplete(function(){
+		     var t = SceneObj.getGeometry(2);
+		     t.rotation.y = Math.PI/8*2;
+		     t.position.z = 0;
+		     t.position.x = -20;
+		     t.scale.x = 0.1;
+		     t.scale.y = 0.1;
+		     t.scale.z = 0.1;
+		     App.scene.add(t);
+		     App.reload();
+		     //motions[9].start();
+		 }));
+    // 8
+    motions.push(new TWEEN.Tween(temp.rotation)
+		 .to({y:-Math.PI/8*2},200)
+		 .easing(TWEEN.Easing.Linear.None)
+		 .onComplete(function(){
+		     /*
+		     var t = SceneObj.getGeometry(4);
+		     t.position.x = 100;
+		     t.position.y = 0;
+		     t.position.z = -120;
+		     t.rotation.y = Math.PI/8*2;
+		     App.scene.add(t);
+		     */
+		 })
+		);
+    // 9
+    motions.push(new TWEEN.Tween(SceneObj.getGeometry(2).scale)
+		 .to({x:1.0,y:1.0,z:1.0},40)
+		 .easing(TWEEN.Easing.Linear.None)
+		 .onStart(function(){
+		     motions[10].start();
+		 })
+		 .onComplete(function(){
+		     // 人間
+		     var t = SceneObj.getGeometry(3);
+		     t.position.x = -20;
+		     t.position.y = -120;
+		     t.rotation.y = Math.PI/8*2;
+		     App.scene.add(t);
+		 }));
+		
+    // 10
+    motions.push(new TWEEN.Tween(SceneObj.getGeometry(3).rotation)
+		 .delay(500)
+		 .to({y:2*Math.PI+Math.PI/8*2},40)
+		 .onStart(function(){
+		     //App.camera.position = new THREE.Vector3(0,500,0);
+		 })
+		 .onComplete(function(){
+		     App.scene.remove(SceneObj.getGeometry(2));
+		     App.scene.remove(SceneObj.getGeometry(3));
+		     //App.scene.remove(SceneObj.getGeometry(4));
+		     temp1.merge(SceneObj.getGeometry(2));
+		     temp1.merge(SceneObj.getGeometry(3));
+		     App.scene.add(temp1);
+		 }));
+    //11
+    motions.push(new TWEEN.Tween(temp1.position)
+		 .to({x:0,y:100},80)
+		 .easing(TWEEN.Easing.Quartic.In)
+		 .onStart(function(){
+		     motions[12].start();
+		 }));
+    //12
+    motions.push(new TWEEN.Tween(temp.position)
+		.to({x:-20,y:100},80)
+		.easing(TWEEN.Easing.Quartic.In));
+    //13
+    motions.push(new TWEEN.Tween(App.camera.position)
+		 .delay(400)
+		 .to({x:120,y:120},1000)
+		 .easing(TWEEN.Easing.Exponential.Out)
+		 .onStart(function(){
+		     var t = SceneObj.getGeometry(4);
+		     t.position.x = 100;
+		     t.position.y = 100;
+		     t.position.z = -120;
+		     t.rotation.y = Math.PI/8*2;
+		     t.scale.x = 0.1;
+		     t.scale.y = 0.1;
+		     t.scale.z = 0.1;
+		     App.scene.add(t);
+		     motions[14].start();
+		     motions[15].start();
+		 }));
+    // 14 
+    motions.push(new TWEEN.Tween(SceneObj.getGeometry(4).scale)
+		 .to({x:1.0,y:1.0,z:1.0},400)
+		 .easing(TWEEN.Easing.Linear.None));
+    //15
+    motions.push(new TWEEN.Tween(App.camera.rotation)
+		 .to({y:Math.PI/8},1000)
+		.onStart(function(){
+		}));
+    /// 16
+    motions.push(new TWEEN.Tween(App.camera.position)
+		 .to({y:-85},500)
+		.onStart(function(){
+		    var t = SceneObj.getGeometry(5);
+		    t.position.x = -1000;
+		    t.position.y = -150;
+		    t.position.z = 1000;
+		    t.rotation.y = Math.PI/8*2;
+		    App.scene.add(t);
+		    //temp1.merge();
+		    motions[17].start();
+		}));
+    //17
+    motions.push(new TWEEN.Tween(SceneObj.getGeometry(5).position)
+		 .to({x:0,z:-20},800)
+		 .easing(TWEEN.Easing.Elastic.Out)
+		 .onComplete(function(){
+		     var t = SceneObj.getGeometry(6);
+		     t.position.x = 0;
+		     t.position.y = -250;
+		     t.position.z = -20;
+		     t.rotation.y = Math.PI/8*2;
+		     t.scale.x = 0.0;
+		     t.scale.y = 0.0;
+		     t.scale.z = 0.0;
+		     App.scene.add(t); 
+		 }));
+    // 18
+    motions.push(new TWEEN.Tween(App.camera.position)
+		 .to({y:-220},500)
+		.onStart(function(){
+		    motions[19].start();
+		    
+		}));
+    // 19
+    motions.push(new TWEEN.Tween(SceneObj.getGeometry(6).scale)
+		 .to({x:1.0,y:2.0,z:1.0},400)
+		 .easing(TWEEN.Easing.Linear.None)
+		.onStart(function(){
+		}));
+    //20
+    motions.push(new TWEEN.Tween(App.camera.position)
+		 .to({x:200,y:-80,z:500},400)
+		 .onStart(function(){
+		     motions[21].start();
+		 }));
+    // 21
+    motions.push(new TWEEN.Tween(App.camera.rotation)
+		 .to({},400));
+    
+    motions[0].chain(motions[2]);
+    motions[2].chain(motions[3]);
+    motions[6].chain(motions[7]);
+    motions[7].chain(motions[9]);
+    motions[10].chain(motions[11]);
+    motions[12].chain(motions[13]);
+    motions[13].chain(motions[16]);
+    motions[16].chain(motions[18]);
+    motions[18].chain(motions[20]);
+    motions[0].start();
     var test2 = (function(){
-	//console.log(temp.position);
-	
 	App.nowAnime = window.requestAnimationFrame(test2);
 	TWEEN.update();
 	App.reload();
@@ -140,12 +370,8 @@
     
 
     // App.anime(test);
-   
-    //temp.rotation.z = Math.PI/2;
     
     /*
-      var temp = SceneObj.AddWord(50,'涼宮ハルヒ',new THREE.Vector3(0,0,0));
-    temp.rotation.z = Math.PI/2;
     var temp = SceneObj.AddWord(30,'ただの',new THREE.Vector3(0,0,0));
     temp.rotation.x = -Math.PI/2;
     //temp.uniforms.fraction.value = 100;
